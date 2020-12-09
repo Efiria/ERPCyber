5adba7d50e1570a69892b210b6db7e27d03004e2
Efiria <bruno.doucet@viacesi.fr>
2020-12-09 10:26:02 +0100

    display user

--- ERPcyber/Model/Database.cs
+++ ERPcyber/Model/Database.cs
@@ -72,8 +72,55 @@ namespace ERPcyber.Model
         public DataTable getEmployee()
         {
 
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=test;";
+            string query = "Select * from stocks";
 
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
 
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                DataTable dataTable = new DataTable();
+                dataTable.Columns.Add("Id");
+                dataTable.Columns.Add("Name");
+                dataTable.Columns.Add("Email");
+                dataTable.Columns.Add("Role");
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        DataRow row = dataTable.NewRow();
+                        row["Id"] = reader.GetString(0);
+                        row["Name"] = reader.GetString(1);
+                        row["Email"] = reader.GetString(2);
+                        row["Role"] = reader.GetString(4);
+                        dataTable.Rows.Add(row);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There is no Stock, please update it.");
+                }
+
+                // Finally close the connection
+                databaseConnection.Close();
+
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
 
 
             return null;
